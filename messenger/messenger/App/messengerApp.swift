4f5a6b7e5cb581c0d268dab104b2d3694081aa5c
onweg <Innokentiydiscord@yandex.ru>
2024-04-09 16:03:29 +0300

    add files

---
+++ messenger/messenger/App/messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerApp.swift
+//  messenger
+//
+//  Created by Пользователь on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
